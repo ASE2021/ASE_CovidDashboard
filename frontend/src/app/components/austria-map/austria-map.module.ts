03071ecf0b3fc339311b48c1282c1fde92239582
TheExecutor <corinnagr@edu.aau.at>
2020-12-13 22:39:36 +0100

    made austrian map view layout more fancy

--- frontend/src/app/components/austria-map/austria-map.module.ts
+++ frontend/src/app/components/austria-map/austria-map.module.ts
@@ -4,16 +4,18 @@ import {AustriaMapComponent} from './austria-map.component';
 import {LeafletModule} from '@asymmetrik/ngx-leaflet';
 import {InputNumberModule} from 'primeng/inputnumber';
 import {FormsModule} from '@angular/forms';
+import {PanelModule} from 'primeng/panel';
 
 
 @NgModule({
   declarations: [AustriaMapComponent],
-  imports: [
-    CommonModule,
-    LeafletModule,
-    InputNumberModule,
-    FormsModule,
-  ],
+    imports: [
+        CommonModule,
+        LeafletModule,
+        InputNumberModule,
+        FormsModule,
+        PanelModule,
+    ],
   exports: [AustriaMapComponent],
 })
 export class AustriaMapModule {
