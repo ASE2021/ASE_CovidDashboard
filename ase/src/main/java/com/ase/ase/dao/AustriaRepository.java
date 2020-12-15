e0c8bd1c77c35613037c49e817078b7d382c6e5c
TheExecutor <corinnagr@edu.aau.at>
2020-12-15 15:42:19 +0100

    added findAllActive and findAllNew Cases methods in AustriaRepository

--- ase/src/main/java/com/ase/ase/dao/AustriaRepository.java
+++ ase/src/main/java/com/ase/ase/dao/AustriaRepository.java
@@ -3,6 +3,15 @@ package com.ase.ase.dao;
 import com.ase.ase.entities.AgeDistribution;
 import com.ase.ase.entities.Austria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AustriaRepository extends JpaRepository<Austria, Long> {
+
+    @Query("Select a.newCases, a.time from Austria a")
+    List<Object[]> findAllNewCases();
+
+    @Query("Select SUM(a.sumCases)-SUM(a.sumCured)-SUM(a.sumCured), a.time from Austria a")
+    List<Object[]> findAllActiveCases();
 }
