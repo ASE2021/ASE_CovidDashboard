09dda0302b57bdfe72a47be7aea45b3ece1235ab
s1lobnig <s1lobnig@edu.aau.at>
2020-12-15 18:13:06 +0100

    deleted unused imports

--- ase/src/main/java/com/ase/ase/dao/DistrictsRepository.java
+++ ase/src/main/java/com/ase/ase/dao/DistrictsRepository.java
@@ -1,6 +1,5 @@
 package com.ase.ase.dao;
 
-import com.ase.ase.entities.AgeDistribution;
 import com.ase.ase.entities.Districts;
 import org.springframework.data.jpa.repository.JpaRepository;
 
--- ase/src/main/java/com/ase/ase/dao/ProvincesRepository.java
+++ ase/src/main/java/com/ase/ase/dao/ProvincesRepository.java
@@ -1,6 +1,5 @@
 package com.ase.ase.dao;
 
-import com.ase.ase.entities.AgeDistribution;
 import com.ase.ase.entities.Provinces;
 import org.springframework.data.jpa.repository.JpaRepository;
 
