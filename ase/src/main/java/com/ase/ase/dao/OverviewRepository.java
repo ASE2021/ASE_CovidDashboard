96c345e7282bed2ef25025a9fc4b6d5c5bf59644
Michael Gubesch <michaelgub@edu.aau.at>
2020-12-17 15:27:04 +0100

    Update OverviewRepository.java
    to get the BasicInformation

--- ase/src/main/java/com/ase/ase/dao/OverviewRepository.java
+++ ase/src/main/java/com/ase/ase/dao/OverviewRepository.java
@@ -4,4 +4,6 @@ import com.ase.ase.entities.Overview;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OverviewRepository extends JpaRepository<Overview, Long> {
+  @Query("SELECT new com.ase.ase.rest.response.BasicInformation(o.sumTested, o.sumCasesConfirmed,o.sumDeadConfirmed) FROM Overview o ")
+    BasicInformation get();
 }
