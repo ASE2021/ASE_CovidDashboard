cf82ec4864ac338c0c5c4f2aef299065b80d1d36
Michael Gubesch <michaelgub@edu.aau.at>
2020-12-17 15:24:49 +0100

    Add files via upload
    Controller for Basic Information should work because there should be only one overview

---
+++ ase/src/main/java/com/ase/ase/rest/controller/BasicInformationController.java
@@ -0,0 +1,27 @@
+package com.ase.ase.rest.controller;
+
+import com.ase.ase.dao.OverviewRepository;
+import com.ase.ase.rest.response.BasicInformation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping(
+        method = RequestMethod.GET,
+        path = "/basicinformation",
+        produces = MediaType.APPLICATION_JSON_VALUE
+)
+public class BasicInformationController {
+
+    @Autowired
+    private OverviewRepository overviewRepository;
+
+    public ResponseEntity getBasicInformation(){
+        BasicInformation b = overviewRepository.get();
+      return ResponseEntity.ok(b);
+  }
+
+}
