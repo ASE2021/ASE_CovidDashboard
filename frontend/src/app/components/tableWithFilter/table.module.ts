67604a4084f7ac064965d80b0ac90db53e10e96e
Szolderits Chiara <c.szolderits@gmail.com>
2021-01-05 08:24:44 +0100

    removed some duplicated lines

--- frontend/src/app/components/tableWithFilter/table.module.ts
+++ frontend/src/app/components/tableWithFilter/table.module.ts
@@ -1,17 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {ChartModule} from 'primeng/chart';
-import {CardModule} from 'primeng/card';
-import {PanelModule} from 'primeng/panel';
 import {TableModule} from 'primeng/table';
-import {SelectButtonModule} from 'primeng/selectbutton';
-import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {TabViewModule} from 'primeng/tabview';
-import {DropdownModule} from 'primeng/dropdown';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {TabMenuModule} from 'primeng/tabmenu';
-import {RoutingModule} from '../../routing.module';
-import {InputTextModule} from 'primeng/inputtext';
+
 import {MultiSelectModule} from 'primeng/multiselect';
 import {FormsModule} from '@angular/forms';
 import {ProgressSpinnerModule} from 'primeng/progressspinner';
@@ -21,18 +11,7 @@ import {TableComponent} from './table.component';
   declarations: [TableComponent],
   imports: [
     CommonModule,
-    ChartModule,
-    CardModule,
-    PanelModule,
-    SelectButtonModule,
-    TabViewModule,
-    NoopAnimationsModule,
-    DropdownModule,
-    BrowserAnimationsModule,
     TableModule,
-    TabMenuModule,
-    RoutingModule,
-    InputTextModule,
     MultiSelectModule,
     FormsModule,
     ProgressSpinnerModule
