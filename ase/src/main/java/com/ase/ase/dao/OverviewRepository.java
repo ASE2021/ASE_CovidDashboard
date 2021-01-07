b803be01db13d7e9812c836587b3bbed6dd6a4c1
Michael Gubesch <michaelgub@edu.aau.at>
2021-01-07 14:21:39 +0100

    Update OverviewRepository.java
    Additional import for succesfull build

--- ase/src/main/java/com/ase/ase/dao/OverviewRepository.java
+++ ase/src/main/java/com/ase/ase/dao/OverviewRepository.java
@@ -1,9 +1,11 @@
 package com.ase.ase.dao;
 
 import com.ase.ase.entities.Overview;
+import com.ase.ase.rest.response.BasicInformation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface OverviewRepository extends JpaRepository<Overview, Long> {
   @Query("SELECT new com.ase.ase.rest.response.BasicInformation(o.sumTested, o.sumCasesConfirmed,o.sumDeadConfirmed) FROM Overview o ")
-    BasicInformation get();
+    BasicInformation getBasic();
 }
