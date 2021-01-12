accfe6ae24f37046a5d21365f9162c7b49aa48bb
s1lobnig <s1lobnig@edu.aau.at>
2021-01-12 14:01:26 +0100

    added query for bed utilization

--- ase/src/main/java/com/ase/ase/dao/BedAndTestTimelineRepository.java
+++ ase/src/main/java/com/ase/ase/dao/BedAndTestTimelineRepository.java
@@ -27,4 +27,22 @@ public interface BedAndTestTimelineRepository extends JpaRepository<BedAndTestTi
             "order by area_id" +
             ") areaItem", nativeQuery = true)
     String getHospitalisationsBy(@Param("areas") Set<Integer> areas);
+
+    @Query(value = "Select CAST(json_build_object('items', json_agg(item)) AS VARCHAR) from (" +
+            "Select json_build_object(" +
+                "'areaId', area_id, " +
+                "'areaName', area, " +
+                "'data', json_agg(json_build_object(" +
+                    "'date', time, " +
+                    "'values', array[" +
+                        "json_build_object('identifier', 'availableBeds', 'value', case when :type = 0 then freeib when :type = 1 then freenb else freeib + freenb end)," +
+                        "json_build_object('identifier', 'utilizedBeds', 'value', case when :type = 0 then usedib when :type = 1 then usednb else usedib + usednb end)" +
+                        "]" +
+                "))) as item " +
+            "from bed_and_test_timeline " +
+            "where area_id in :areas " +
+            "group by area_id, area " +
+            "order by area_id" +
+            ") areaItem", nativeQuery = true)
+    String getBedUtilizationBy(@Param("areas") Set<Integer> areas, @Param("type") int type);
 }
