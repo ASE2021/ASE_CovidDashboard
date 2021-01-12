77a4f0c108886124fe7fff832fe780aff09d250c
s1lobnig <s1lobnig@edu.aau.at>
2021-01-12 12:32:17 +0100

    added query for age-sex-deaths distribution

--- ase/src/main/java/com/ase/ase/dao/SexAndAgeDistributionRepository.java
+++ ase/src/main/java/com/ase/ase/dao/SexAndAgeDistributionRepository.java
@@ -28,5 +28,26 @@ public interface SexAndAgeDistributionRepository extends JpaRepository<SexAndAge
             "where area_id in :areas " +
             "group by dataItems.area_id " +
             ") areaItems", nativeQuery = true)
-    String getSexAndAgeDistributionBy(@Param("areas")Set<Integer> areas);
+    String getSexAndAgeCaseDistributionBy(@Param("areas")Set<Integer> areas);
+
+    @Query(value = "Select CAST(json_build_object('items', json_agg(item)) AS VARCHAR) from (" +
+            "select json_build_object('areaId', dataItems.area_id, 'data', json_agg(dataItems.dataItem)) as item " +
+            "from (" +
+                "select area_id, " +
+                    "json_build_object(" +
+                        "'ageIntervalId', age_interval_id, " +
+                        "'ageInterval', age_interval, " +
+                        "'values', json_agg(case when sex = 'M' " +
+                            "then json_build_object('identifier', 'maleDeaths', 'value', sum_dead) " +
+                            "else json_build_object('identifier', 'femaleDeaths', 'value', sum_dead) " +
+                            "end)" +
+                    ") as dataItem " +
+                "from sex_and_age_distribution " +
+                "group by area_id, age_interval_id, age_interval " +
+                "order by area_id, age_interval_id" +
+            ") dataItems " +
+            "where area_id in :areas " +
+            "group by dataItems.area_id " +
+            ") areaItems", nativeQuery = true)
+    String getSexAndAgeDeathDistributionBy(@Param("areas")Set<Integer> areas);
 }
