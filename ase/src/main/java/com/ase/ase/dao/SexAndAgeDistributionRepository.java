13332a6921405e7a8847d18e3a818993ba074335
s1lobnig <s1lobnig@edu.aau.at>
2021-01-12 12:26:52 +0100

    added query for age-sex-cases distribution

--- ase/src/main/java/com/ase/ase/dao/SexAndAgeDistributionRepository.java
+++ ase/src/main/java/com/ase/ase/dao/SexAndAgeDistributionRepository.java
@@ -2,6 +2,31 @@ package com.ase.ase.dao;
 
 import com.ase.ase.entities.SexAndAgeDistribution;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Set;
 
 public interface SexAndAgeDistributionRepository extends JpaRepository<SexAndAgeDistribution, Long> {
+
+    @Query(value = "Select CAST(json_build_object('items', json_agg(item)) AS VARCHAR) from (" +
+            "select json_build_object('areaId', dataItems.area_id, 'data', json_agg(dataItems.dataItem)) as item " +
+            "from (" +
+                "select area_id, " +
+                    "json_build_object(" +
+                        "'ageIntervalId', age_interval_id, " +
+                        "'ageInterval', age_interval, " +
+                        "'values', json_agg(case when sex = 'M' " +
+                            "then json_build_object('identifier', 'maleCases', 'value', sum_cases) " +
+                            "else json_build_object('identifier', 'femaleCases', 'value', sum_cases) " +
+                            "end)" +
+                    ") as dataItem " +
+                "from sex_and_age_distribution " +
+                "group by area_id, age_interval_id, age_interval " +
+                "order by area_id, age_interval_id" +
+            ") dataItems " +
+            "where area_id in :areas " +
+            "group by dataItems.area_id " +
+            ") areaItems", nativeQuery = true)
+    String getSexAndAgeDistributionBy(@Param("areas")Set<Integer> areas);
 }
