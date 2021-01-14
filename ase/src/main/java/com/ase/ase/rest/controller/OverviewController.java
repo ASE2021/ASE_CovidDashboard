a78cc8f95d72827c0414ca8ad711f8ca8d635e88
Michael Gubesch <michaelgub@edu.aau.at>
2021-01-14 11:37:24 +0100

    Update OverviewController.java
    First Change in the Controller

--- ase/src/main/java/com/ase/ase/rest/controller/OverviewController.java
+++ ase/src/main/java/com/ase/ase/rest/controller/OverviewController.java
@@ -2,25 +2,20 @@ package com.ase.ase.rest.controller;
 
 import com.ase.ase.dao.CasesTimelineRepository;
 import com.ase.ase.rest.response.Overview;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping(
-        method = RequestMethod.GET,
-        path = "/overview",
-        produces = MediaType.APPLICATION_JSON_VALUE
-)
+@RequestMapping("/overview")
 public class OverviewController {
 
-    @Autowired
-    private CasesTimelineRepository casestimelineRepository;
+    @GetMapping(value = "/overview", produces = "application/json")
+    @ResponseBody
 
     public ResponseEntity getOverview(){
-        Overview o = casestimelineRepository.getOverview();
+        Overview o = CasesTimelineRepository.getOverview();
       return ResponseEntity.ok(o);
   }
 
