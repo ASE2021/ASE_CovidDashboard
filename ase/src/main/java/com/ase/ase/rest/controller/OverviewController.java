10da43613e408979761306586dc779905d3adaae
Michael Gubesch <michaelgub@edu.aau.at>
2021-01-14 13:55:04 +0100

    Update OverviewController.java
    next try

--- ase/src/main/java/com/ase/ase/rest/controller/OverviewController.java
+++ ase/src/main/java/com/ase/ase/rest/controller/OverviewController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping(method = { RequestMethod.GET })
 public class OverviewController {
 
     @GetMapping(value = "/overview", produces = "application/json")
