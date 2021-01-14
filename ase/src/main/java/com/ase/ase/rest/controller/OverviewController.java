859bb2273782a5a7108e500006afdc4183c0a294
Michael Gubesch <michaelgub@edu.aau.at>
2021-01-14 12:51:29 +0100

    Update OverviewController.java
    change the Controller again

--- ase/src/main/java/com/ase/ase/rest/controller/OverviewController.java
+++ ase/src/main/java/com/ase/ase/rest/controller/OverviewController.java
@@ -1,5 +1,6 @@
 package com.ase.ase.rest.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import com.ase.ase.dao.CasesTimelineRepository;
 import com.ase.ase.rest.response.Overview;
 import org.springframework.http.MediaType;
@@ -13,9 +14,12 @@ public class OverviewController {
 
     @GetMapping(value = "/overview", produces = "application/json")
     @ResponseBody
+    
+    @Autowired
+    private CasesTimelineRepository casesTimelineRepository;
 
     public ResponseEntity getOverview(){
-        String o = CasesTimelineRepository.getOverview();
+        String o = casesTimelineRepository.getOverview();
       return ResponseEntity.ok(o);
   }
 
