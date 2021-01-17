9acdf64f7427310bac079dae7fb919b312f6e41a
Michael Gubesch <michaelgub@edu.aau.at>
2021-01-17 22:57:48 +0100

    Create HospitalController.java
    set HospitalController

---
+++ ase/src/main/java/com/ase/ase/rest/controller/HospitalController.java
@@ -0,0 +1,22 @@
+package com.ase.ase.rest.controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/hospital-bed-utilizations")
+public class HospitalController {
+
+    @Autowired
+    private BedAndTestTimelineRepository bedandTestTimelineRepository;
+
+    @CrossOrigin
+    @GetMapping(value = "?{area-id}&{type}", produces = "application/json")
+    @ResponseBody
+    public ResponseEntity getBedUtilizationBy(@PathVariable("area-id") Set<Integer> areas, @PathVariable("type") int type) {
+        String b = bedandTestTimelineRepository.getBedUtilizationBy(areas,type);
+        return ResponseEntity.ok(b);
+    }
+}
