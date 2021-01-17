f0b7b95649ce4351db5bb937234dca7760ca2227
Michael Gubesch <michaelgub@edu.aau.at>
2021-01-17 23:04:59 +0100

    Update OverviewController.java
    update Controller

--- ase/src/main/java/com/ase/ase/rest/controller/OverviewController.java
+++ ase/src/main/java/com/ase/ase/rest/controller/OverviewController.java
@@ -2,6 +2,7 @@ package com.ase.ase.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import com.ase.ase.dao.CasesTimelineRepository;
+import com.ase.ase.dao.PopulationRepository;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ public class OverviewController {
    
     @Autowired
     private CasesTimelineRepository casesTimelineRepository;
+    private PopulationRepository populationRepository;
     
     @CrossOrigin
     @GetMapping(value = "/overview", produces = "application/json")
@@ -20,4 +22,22 @@ public class OverviewController {
         String o = casesTimelineRepository.getOverview();
             return ResponseEntity.ok(o);
     }
+
+    @CrossOrigin
+    @GetMapping(value = "/provinces", produces = "application/json")
+    @ResponseBody
+    public ResponseEntity getAllProvincesAndAustria(){
+        String p = populationRepository.getAllProvincesAndAustria();
+            return ResponseEntity.ok(p);
+            
+    }
+
+    @CrossOrigin
+    @GetMapping(value = "/districs", produces = "application/json")
+    @ResponseBody
+    public ResponseEntity getAllDistricts(){
+        String d = populationRepository.getAllDistricts();
+            return ResponseEntity.ok(d);
+    }
+    
 }
