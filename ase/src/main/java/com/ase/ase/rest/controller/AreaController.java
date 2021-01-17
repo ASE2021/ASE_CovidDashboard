4540019b4740fdd42e2070f6bfb0c42f062c096a
Michael Gubesch <michaelgub@edu.aau.at>
2021-01-17 22:56:10 +0100

    Create AreaController.java
    Create AreaController

---
+++ ase/src/main/java/com/ase/ase/rest/controller/AreaController.java
@@ -0,0 +1,34 @@
++package com.ase.ase.rest.controller;
+
+import com.ase.ase.dao.CasesTimelineRepository;
+import com.ase.ase.rest.response.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/area-info")
+public class AreaController {
+  
+    @Autowired
+    private CasesTimelineRepository casesTimelineRepository;
+
+    @CrossOrigin
+    @GetMapping(value = "?{area-id}&{relative}", produces = "application/json")
+    @ResponseBody
+    public ResponseEntity getRelativeAreaInfoBy(@PathVariable("area-id") Set<Integer> areas, @PathVariable("relative") boolean relative){
+        String o ="";
+        if(relative == true){
+            o = casesTimelineRepository.getRelativeAreaInfoBy();
+        } else {
+            o = casesTimelineRepository.getAreaInfoBy();
+        }
+        return ResponseEntity.ok(o);
+    }
+}
